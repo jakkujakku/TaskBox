37647d863585d0fc38a84c4ba2deb4e2e99d0c2d
jakkujakku <jakkujakku98@gmail.com>
2023-12-13 21:52:09 +0900

    init: TaskBox 프로젝트 생성

---
+++ TaskBox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskBox
+//
+//  Created by 준우의 MacBook 16 on 12/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskBox/TaskBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskBoxApp.swift
+//  TaskBox
+//
+//  Created by 준우의 MacBook 16 on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
